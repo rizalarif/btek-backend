1f820e377484250ba7f737f47c9b108e3ae9903a
rizalarif <rizal.arif2001@gmail.com>
2022-11-08 20:04:27 +0700

    supabase

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,7 +1,7 @@
 const {Pool} = require("pg");
 
 const db = new Pool({
-  connectionString: "postgresql://postgres:1@localhost:5432/postgres?schema=public"
+  connectionString: "postgresql://postgres:RizalArif_2001@db.dsvmzobpguwbpfmjdwxw.supabase.co:5432/postgres?schema=public"
 });
 
 module.exports = db;
